9b8f0b692f8a378bd40c6f995333f2f2b9786559
mazurkevych30 <132198562+mazurkevych30@users.noreply.github.com>
2023-11-20 09:35:03 +0000

    second and tird tasks

---
+++ src/js/02-timer.js
@@ -0,0 +1,100 @@
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css"
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css"
+
+const date = new Date();
+let ms;
+
+iziToast.settings({
+    timeout: 1000,
+    icon: "",
+});
+
+const options = {
+enableTime: true,
+time_24hr: true,
+defaultDate: new Date(),
+minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] > date) {
+            elements.btnStart.disabled = false;
+            ms = selectedDates[0] - date;
+        }
+        else {
+            elements.btnStart.disabled = true;
+            iziToast.error({
+                message: '"Please choose a date in the future"',
+                position: 'topRight',
+            });
+        }
+},
+};
+
+const elements = {
+    dateInput: document.querySelector("#datetime-picker"),
+    btnStart: document.querySelector("[data-start]"),
+    days: document.querySelector("[data-days]"),
+    hours: document.querySelector("[data-hours]"),
+    minutes: document.querySelector("[data-minutes]"),
+    seconds: document.querySelector("[data-seconds]"),
+}
+
+
+elements.btnStart.disabled = true;
+elements.btnStart.addEventListener("click", handlerDate)
+
+flatpickr(elements.dateInput, options);
+
+function handlerDate() {
+    elements.btnStart.disabled = true;
+    elements.dateInput.disabled = true;
+    const timerId = setInterval(() => {
+        ms -= 1000;
+
+        if (ms<0) {
+            clearInterval(timerId);
+            return;
+        }
+
+        const convertDate = convertMs(ms);
+        markup(convertDate)
+
+    }, 1000)
+}
+
+function markup(convertDate) {
+    elements.days.textContent = `${addLeadingZero(convertDate.days)}`;
+    elements.hours.textContent = `${addLeadingZero(convertDate.hours)}`;
+    elements.minutes.textContent = `${addLeadingZero(convertDate.minutes)}`;
+    elements.seconds.textContent = `${addLeadingZero(convertDate.seconds)}`;
+}
+
+function addLeadingZero(value) {
+    value += "";
+    if (value.length < 2) {
+        return value.padStart(2, 0);
+    }
+    
+    return value;
+}
+
+function convertMs(ms) {
+// Number of milliseconds per unit of time
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+// Remaining days
+const days = Math.floor(ms / day);
+// Remaining hours
+const hours = Math.floor((ms % day) / hour);
+// Remaining minutes
+const minutes = Math.floor(((ms % day) % hour) / minute);
+// Remaining seconds
+const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,66 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css"
+
+
+iziToast.settings({
+  timeout: 5000,
+  position: 'topRight',
+  icon: "",
+  transitionIn: "fadeInRight",
+  transitionOut: "fadeOutLeft",
+  close: false,
+});
+
+
+const form = document.querySelector(".form");
+form[3].addEventListener("click", onClick)
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  let delayStart = Number(form[0].value);
+  const step = Number(form[1].value);
+  const amount = Number(form[2].value);
+  const promises = [];
+
+  for (let i = 1; i <= amount; i += 1){
+     if (i > 1) {
+      delayStart += step;
+    }
+    promises.push(createPromise(i, delayStart))
   }
+  
+  Promise.allSettled(promises)
+    .then((items) => {
+      items.forEach((item) => {
+        let delayPromise = item.value ? item.value.delay : item.reason.delay;
+        setTimeout(() => {
+          if (item.status === "fulfilled") {
+            iziToast.success({
+              message: `✅ Fulfilled promise ${item.value.position} in ${delayPromise}ms`,
+            });
+          } else {
+            iziToast.error({
+              message: `❌ Rejected promise ${item.reason.position} in ${delayPromise}ms`,
+            });
+          }
+        }, delayPromise);
+      })
+    })
+    .catch((err) => {
+    console.log(err);
+  })
+}
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    resolve({position, delay})
+  } else {
+    reject({position, delay})
+  }
+  })
 }
